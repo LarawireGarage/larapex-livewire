8ad3d7e0cdd3209bf9e58c6ff7f7eac50a79ed75
Nirmal <88931913+LarawireGarage@users.noreply.github.com>
2023-12-10 22:03:09 +0530

    changes
    Signed-off-by: Nirmal <88931913+LarawireGarage@users.noreply.github.com>

--- src/LivewireChartComponent.php
+++ src/LivewireChartComponent.php
@@ -10,7 +10,7 @@ abstract class LivewireChartComponent extends Component
 {
     use HasChart;
 
-    /** @var \LarawireGarage\LarapexLivewire\wireables\LarapexWirable $chart */
+    /** @var \LarawireGarage\LarapexLivewire\Wireable\LarapexWirable $chart */
     protected $chart;
 
     /** initialize on mount */
@@ -27,12 +27,17 @@ abstract class LivewireChartComponent extends Component
     #[On('update:chart')]
     public function updateChart(...$params)
     {
+        // dd('update:chart');
         if (is_array($params[0])) {
             $this->hydrateInstanceProperties($params[0]);
+            // $this->dispatch('larapex-chart-update');
             return;
         }
 
         $this->hydrateInstanceProperties($params);
+        // $this->dispatch('larapex-chart-update');
+
+
     }
 
     private function hydrateInstanceProperties(array $properties = [])
@@ -47,7 +52,7 @@ abstract class LivewireChartComponent extends Component
     public function render()
     {
         $this->build();
-        $this->options = $this->chart->getOptionsAsJson();
+        $this->options = $this->chart->getOptionsAsArray();
 
         return view('larapex-livewire::chart-component', [
             'chart' => $this->chart,
